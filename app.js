d69493756b7140eb3b3c64ab5091f2dbc965530b
prafulwalurkar <159822878+prafulwalurkar@users.noreply.github.com>
2024-04-09 18:56:39 +0530

    ten comiit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("../listing.js");
+const Listing = require("../models/listing.js");
 
 
 
