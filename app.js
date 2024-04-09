131b93665f6e712ba4c9922b43740a5a5ed3adfc
prafulwalurkar <159822878+prafulwalurkar@users.noreply.github.com>
2024-04-09 18:39:47 +0530

    ten comiit

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("../listing.js");
+
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main().then(()=>{
     console.log("Connect to DB");
 }).catch((err)=>{
-    console.log(err);
+    console.log(err123);
 });
+
+
 async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
 app.get("/",(req,res)=>{
     res.send("Hi I am root");
+});
+
+
+app.get("/testListing", (req, res)=> {
+
 })
 
 app.listen(8080, ()=>{
---
+++ models/listing.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title : {
+        type : String,
+        require : true,
+    }
+    description : String,
+    image : {
+        type : String,
+        set: (v) =>  v === "" ? "https://media.istockphoto.com/id/184103864/photo/clouds-on-sky.jpg?s=1024x1024&w=is&k=20&c=n09_q789C4LPvey3op74SGkt3-OgmR8r-giT5jiEWeA=",:v,
+    price : Number,
+    location : String,
+    country : String,
+});
+
+const Listing =mongoose.model("Listing",listingschema);
+module.export = Listing;
