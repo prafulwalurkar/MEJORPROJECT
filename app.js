e6841337e7c76a44297997eccc96eea7a3543525
prafulwalurkar <159822878+prafulwalurkar@users.noreply.github.com>
2024-04-19 18:18:46 +0530

    13 comiit

--- app.js
+++ app.js
@@ -27,17 +27,58 @@ app.get("/",(req,res)=>{
     res.send("Hi I am root");
 });
 
-// index routing
+//Index Route
 app.get("/listings",async(req,res) =>{
     const allListings = await Listing.find({});
     res.render("./listings/index.ejs", {allListings});
 });
 
+//New Route
+app.get("/listings/new",async(req, res)=>{
+    res.render("./listing/new.ejs",{listing});
+});
+
 //show routing
 app.get("/listings/:id", async (req, res) => {
-    let (id) = req.params;
+    let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/show.ejs", { listing });
+
+});
+
+//Create Route
+
+app.post("/listings",async (req, res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("./listings");
+});
+
+//Edit Route
+
+app.get("/listings/:id/edit", async(req,res)=>{
+    let { id }=req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs", {listing});
+
+});
+
+//Update Route
+
+app.put("/listings/:id", async (req,res)=> {
+    let { id }=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect("./listings/${id}");
+
+});
+
+//Delete Route
+
+app.delete("/listings/:id", async(req, res)=>{
+    let {id}= req.params;
+    let deletedListing =await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("./listings");
 
 });
 
