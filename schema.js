605bb4504d32d96cfe6f6b9edf23c1990aee3757
prafulwalurkar <159822878+prafulwalurkar@users.noreply.github.com>
2024-06-20 01:43:38 +0530

    add schema

--- schema.js
+++ schema.js
@@ -1,21 +1,22 @@
-const Joi = require("joi");
+const Joi = require('joi');
+
 const review = require("./models/review");
 
 
 module.exports.listingSchema = Joi.object({
     listing: Joi.object({
-        title: Joi.string().require(),
-        discription: Joi.string().require(),
-        location: Joi.string().require(),
-        country: Joi.string().require(),
-        price: Joi.number().require().min(0),
+        title: Joi.string().required(),
+        discription: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required().min(0),
         image: Joi.string().allow("",null),
-    }).require(),
+    }).required(),
     });  
     
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
-        rating: Joi.number().require().min(1).max(5),
-        comment: Joi.string().require()
-    }).require()
+        rating: Joi.number().required().min(1).max(5 ),
+        comment: Joi.string().required()
+    }).required()
 });
